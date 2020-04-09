7ac33c4fea60fe6526da1582b4cc53752c044278
rphila <rphila@gmail.com>
2020-04-08 20:42:28 -0700

    Add call to google translate api

--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from pytesseract import pytesseract as pt
 from flask import Flask, render_template, url_for, make_response, request, flash, redirect
+from google.cloud import translate_v2 as translate
 import datetime
 import os
 import codecs
@@ -18,12 +19,13 @@ def home():
         img_text = request.files['imgtext']
 
         img_text.save(os.path.join("/app/temp", 'img.jpg'))
-        trans_text = ocr()
+        ocr_text = perform_ocr()
+        trans_text = translate_text(ocr_text)
        # return redirect('/')
-    return render_template("input.html", trans = trans_text)
+    return render_template("input.html", trans=trans_text)
 
 
-def ocr():
+def perform_ocr():
     pt.run_tesseract('/app/temp/img.jpg', 'file_ocr',  extension=".jpg", lang='lao') #'eng')
 
     txt = ""
@@ -38,6 +40,25 @@ def ocr():
   #  f.close()
   
     return txt
+
+
+
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="c:/dev/laolessons/lao-translation-app/GoogleTranslate Project-4778da0a83ca.json"
+#print(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
+
+def translate_text(text):
+    translate_client = translate.Client()
+
+   # text="ສະບາຍດີ "
+    result = translate_client.translate(
+        text, target_language="en")
+
+   # print(u'Text: {}'.format(result['input']))
+   # print(u'Translation: {}'.format(result['translatedText']))
+    #print(u'Detected source language: {}'.format(
+    #    result['detectedSourceLanguage']))
+
+    return result['translatedText']
 
 
 if __name__ == "__main__":
